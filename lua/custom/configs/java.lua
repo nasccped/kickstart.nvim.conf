09b3864d8cbfeba8cb8c3740dccb6b772e533b90
Nasccped <pedro.main.tec@gmail.com>
2025-06-06 14:49:20 -0300

    fix(BufferError): don't try to convert tab escapes (error occurs when tab not found)

--- lua/custom/configs/java.lua
+++ lua/custom/configs/java.lua
@@ -9,22 +9,6 @@ function M.setup()
       vim.bo.tabstop = 4 -- Width of tab character
       vim.bo.softtabstop = 4 -- Number of spaces inserted for a tab
       vim.bo.shiftwidth = 4 -- Number of spaces for autoindent
-
-      -- converting existing tabs to spaces on save
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        buffer = 0,
-        callback = function()
-          -- Only convert if expandtab is enabled
-          if vim.bo.expandtab then
-            -- Save cursor position
-            local cursor_pos = vim.api.nvim_win_get_cursor(0)
-            -- Replace all tabs with spaces in the entire file
-            vim.cmd [[%s/\t/    /g]]
-            -- Restore cursor position
-            vim.api.nvim_win_set_cursor(0, cursor_pos)
-          end
-        end,
-      })
     end,
   })
 end
