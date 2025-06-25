359c0d001219059b9a2bbece2c245d7e79138e89
Nasccped <pedro.main.tec@gmail.com>
2025-06-25 18:45:20 -0300

    chore: aadd catppuccin plugin

--- init.lua
+++ init.lua
@@ -256,6 +256,11 @@ require('lazy').setup({
     lazy = false,
   },
 
+  -- NOTE: add catppuccin plugin
+  {
+    'catppuccin/nvim',
+  },
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
