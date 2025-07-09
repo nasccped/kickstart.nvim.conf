64abc2ff59ba7a90d99f67c4b3978c5bde985948
Nasccped <pedro.main.tec@gmail.com>
2025-07-09 10:47:41 -0300

    chore: add catppuccin colorscheme + set dark-blue bg

---
+++ lua/custom/plugins/colorscheme/catppuccin.lua
@@ -0,0 +1,14 @@
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  opts = {
+    color_overrides = {
+      mocha = {
+        base = '#0b0b0f',
+        mantle = '#0b0b0f',
+        crust = '#0b0b0f',
+      },
+    },
+  },
+}
