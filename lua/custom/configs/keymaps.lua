db3f18c5c54c769486f6d15032223c7172af1d5c
Nasccped <pedro.main.tec@gmail.com>
2025-07-10 20:30:35 -0300

    chore: add comments per mapping section

--- lua/custom/configs/keymaps.lua
+++ lua/custom/configs/keymaps.lua
@@ -1,4 +1,7 @@
 local keymap = vim.keymap.set
 
+-- new related mappings
 keymap('n', '<leader>nt', ':term<CR>i', { desc = 'Open a terminal in a new buffer' })
+
+-- close related mappings
 keymap('n', '<leader>cb', ':bd<CR>', { desc = 'Close the current buffer' })
