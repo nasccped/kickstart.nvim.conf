71037b6208318146548cd3a6213e30f6c7a7af19
Nasccped <pedro.main.tec@gmail.com>
2025-07-10 20:56:47 -0300

    fix: ensure not closing tab when buffer closes

--- lua/custom/configs/keymaps.lua
+++ lua/custom/configs/keymaps.lua
@@ -4,4 +4,4 @@ local keymap = vim.keymap.set
 keymap('n', '<leader>ns', ':term<CR>i', { desc = 'Open a new shell (terminal) in the current tab' })
 
 -- close related mappings
-keymap('n', '<leader>cb', ':bd<CR>', { desc = 'Close the current buffer' })
+keymap('n', '<leader>cb', ':bp|bd#<CR>', { desc = "Close the current buffer (don't close the tab)" })
