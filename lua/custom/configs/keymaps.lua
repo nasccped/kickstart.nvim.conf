1669f868a648e8079344c0953d532a88cb8d1667
Nasccped <pedro.main.tec@gmail.com>
2025-07-10 21:08:17 -0300

    chore: add tab related mappings

--- lua/custom/configs/keymaps.lua
+++ lua/custom/configs/keymaps.lua
@@ -2,6 +2,12 @@ local keymap = vim.keymap.set
 
 -- new related mappings
 keymap('n', '<leader>ns', ':term<CR>i', { desc = 'Open a new shell (terminal) in the current tab' })
+keymap('n', '<leader>nt', ':tabnew<CR>', { desc = 'Open a new tab' })
 
 -- close related mappings
 keymap('n', '<leader>cb', ':bp|bd#<CR>', { desc = "Close the current buffer (don't close the tab)" })
+keymap('n', '<leader>ct', ':tabclose<CR>', { desc = 'Close the current tab' })
+
+-- tab-moving related mappings
+keymap('n', '<Tab>', ':tabnext<CR>', { desc = 'Move to the next open tab' })
+keymap('n', '<S-Tab>', ':tabprevious<CR>', { desc = 'Move to the previous open tab' })
